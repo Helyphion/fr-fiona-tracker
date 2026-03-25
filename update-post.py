dde4483ecb80ad6778427e8c5b4b4ed5877ae0d5
Helyphion <helyphion@gmail.com>
2026-03-26 00:31:12 +0100

    Change to YAML parsing

--- update-post.py
+++ update-post.py
@@ -1,9 +1,10 @@
+import yaml
 from rich import print
 
 
 try:
     with open("update-data.yaml", "r") as file:
-        info = file.read().splitlines()
+        info = yaml.safe_load(file)
 except FileNotFoundError:
     with open("update-data.yaml", "w") as file:
         file.write('announcement url: \nfr date: \n\nspacing:\n  - goes after: ""\n    change: ""\n  - goes after: ""\n    change: ""\n\nfamiliars:\n  - feat fam: \n    req fam 1: \n    req fam 2: \n    source: \n    goes after: \n\n  - feat fam: \n    req fam 1: \n    req fam 2: \n    source: \n    goes after: ')
