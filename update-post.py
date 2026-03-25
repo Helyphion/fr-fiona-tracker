aac0a3ea60906987d03d4238f50a24b58e187eb6
Helyphion <helyphion@gmail.com>
2026-03-26 00:28:19 +0100

    Switch input to YAML, automatically create file if missing

--- update-post.py
+++ update-post.py
@@ -1,10 +1,12 @@
 from rich import print
 
+
 try:
-    with open("update-data.txt", "r") as file:
+    with open("update-data.yaml", "r") as file:
         info = file.read().splitlines()
 except FileNotFoundError:
-    with open("update-data.txt", "w") as file:
-        file.write("F_ID\nF_Primary\nF_Secondary\nF_Tertiary\n\nM_ID\nM_Primary\nM_Secondary\nM_Tertiary")
-    print("[bold yellow]No input.txt found. File has been created; please write your data into it.[/bold yellow]")
+    with open("update-data.yaml", "w") as file:
+        file.write('announcement url: \nfr date: \n\nspacing:\n  - goes after: ""\n    change: ""\n  - goes after: ""\n    change: ""\n\nfamiliars:\n  - feat fam: \n    req fam 1: \n    req fam 2: \n    source: \n    goes after: \n\n  - feat fam: \n    req fam 1: \n    req fam 2: \n    source: \n    goes after: ')
+    print("[bold yellow]No update-data.yaml found. File has been created; please write your data into it.[/bold yellow]")
     raise
+
