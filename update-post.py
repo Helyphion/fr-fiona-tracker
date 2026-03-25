06f0202a242dcb8f95256f885d53e7f95fed26d4
Helyphion <helyphion@gmail.com>
2026-03-26 00:39:24 +0100

    Get started on YAML parsing

--- update-post.py
+++ update-post.py
@@ -11,3 +11,17 @@ except FileNotFoundError:
     print("[bold yellow]No update-data.yaml found. File has been created; please write your data into it.[/bold yellow]")
     raise
 
+
+announcementURL = info["announcement url"]
+FRdate = info["fr date"]
+
+spacing = info["spacing"]
+familiars = info["familiars"]
+
+print(announcementURL)
+print(FRdate)
+print(spacing)
+print(familiars)
+# check number of entries using len(familiars) etc
+# also disregard entries with blank (None) fields just in case
+# then assemble BBCode list by iterating based on the number of valid entries
