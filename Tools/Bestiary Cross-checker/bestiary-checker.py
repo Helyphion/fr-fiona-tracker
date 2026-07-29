dfa591e61cda3225455a60d7696f00939b3a7c3e
Helyphion <helyphion@gmail.com>
2026-07-29 05:26:53 +0200

    Finish script functionality

--- Tools/Bestiary Cross-checker/bestiary-checker.py
+++ Tools/Bestiary Cross-checker/bestiary-checker.py
@@ -1,5 +1,13 @@
-# awakened
-#  https://www1.flightrising.com/bestiary/676173/1?view=all&filter=true&location=&bond_level=awakened&name=&sort=name_asc&limit=60&display=compact
+# owned
+# already excludes awakened ones
+# https://www1.flightrising.com/bestiary/676173?view=discovered&filter=true&location=&bond_level=not-awakened&name=&sort=name_asc&limit=60&display=compact
+
+# or.. hoard
+# https://www1.flightrising.com/bestiary/676173/1?view=discovered&filter=true&location=hoard&bond_level=not-awakened&name=&sort=name_asc&limit=60&display=compact
+# + vault
+# https://www1.flightrising.com/bestiary/676173?view=discovered&filter=true&location=vault&bond_level=not-awakened&name=&sort=name_asc&limit=60&display=compact
+# ? would make the in progress check obsolete, might be smarter..
+# neeevermind, duplicates still show up. so not obsolete; but still less to copy if going with these two links
 
 # in progress
 # https://www1.flightrising.com/bestiary/676173?view=all&filter=true&location=lair&bond_level=not-awakened&name=&sort=name_asc&limit=60&display=compact
@@ -7,46 +15,44 @@
 # feats list
 # https://www1.flightrising.com/forums/gde/3416901#post_58979490
 
-
-with open("awakened.txt", "r") as file:
-    awakenedFams = file.read().splitlines()
+# TODO: clean up links
 
 
-# remove nicknames from awakened list
-for fam in awakenedFams:
-    if fam[0] == '“':
-        awakenedFams.remove(fam)
+def removeNicknames(list):
+    for fam in list:
+        if fam[0] == '“':
+            list.remove(fam)
 
-
+with open("owned.txt", "r") as file:
+    ownedFams = file.read().splitlines()
 
 with open("in-progress.txt", "r") as file:
     equippedFams = file.read().splitlines()
 
-# remove nicknames from equipped list
-for fam in equippedFams:
-    if fam[0] == '“':
-        equippedFams.remove(fam)
+removeNicknames(ownedFams)
+removeNicknames(equippedFams)
 
 
 
 with open("feats-list.txt", "r") as file:
     featsList = file.read().splitlines()
 
-
 # remove blank lines
 while "" in featsList:
     featsList.remove("")
 
+# remove headings, spacers (_), and notes in parentheses
 cleanFeatsList = []
-
 for line in featsList:
 
     if not line.isupper() and not line[0] == "_":
-        
+
         if line[-1:] == ")":
             line = line[:line.index("(")-1]
 
         cleanFeatsList.append(line)
     
 
-print(cleanFeatsList)
+for x in cleanFeatsList:
+    if x in ownedFams and x not in equippedFams:
+        print(x)
