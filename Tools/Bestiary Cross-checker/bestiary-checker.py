127cab520400082c8337d2a32d550d1901f6f3a4
Helyphion <helyphion@gmail.com>
2026-07-29 06:24:17 +0200

    Add user ID prompt and instructions, plus logic for showing them when input files are missing

--- Tools/Bestiary Cross-checker/bestiary-checker.py
+++ Tools/Bestiary Cross-checker/bestiary-checker.py
@@ -1,37 +1,73 @@
-# owned
-# already excludes awakened ones
-# https://www1.flightrising.com/bestiary/676173?view=discovered&filter=true&location=&bond_level=not-awakened&name=&sort=name_asc&limit=60&display=compact
+import sys
 
-# or.. hoard
-# https://www1.flightrising.com/bestiary/676173/1?view=discovered&filter=true&location=hoard&bond_level=not-awakened&name=&sort=name_asc&limit=60&display=compact
-# + vault
-# https://www1.flightrising.com/bestiary/676173?view=discovered&filter=true&location=vault&bond_level=not-awakened&name=&sort=name_asc&limit=60&display=compact
-# ? would make the in progress check obsolete, might be smarter..
-# neeevermind, duplicates still show up. so not obsolete; but still less to copy if going with these two links
+try:
+    
+    with open("owned.txt", "r") as file:
+        ownedFams = file.read().splitlines()
 
-# in progress
-# https://www1.flightrising.com/bestiary/676173?view=all&filter=true&location=lair&bond_level=not-awakened&name=&sort=name_asc&limit=60&display=compact
+    with open("in-progress.txt", "r") as file:
+        equippedFams = file.read().splitlines()
 
-# feats list
-# https://www1.flightrising.com/forums/gde/3416901#post_58979490
+except:
+    userId = input("Enter your user ID: ")
 
-# TODO: clean up links
+    if userId.startswith("#"):
+        userId = userId[1:]
 
+    if not userId.isdigit():
+        sys.exit("Error: User ID needs to be numbers only. (And maybe a hashtag at the start, I accounted for that part.)")
+
+
+    with open("owned.txt", "w") as file:
+        file.write("")
+
+    #with open("in-progress.txt", "w") as file:
+    #    file.write("")
+
+    instructions = f"""
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+FOR MORE READABLE INSTRUCTIONS (BUT WITHOUT CUSTOMISED LINKS), CHECK HERE:
+https://github.com/Helyphion/fr-fiona-tracker/blob/main/Tools/Bestiary%20Cross-checker/instructions.md
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+INSTRUCTIONS:
+Open https://www1.flightrising.com/bestiary/{userId}?view=discovered&filter=true&location=hoard&bond_level=not-awakened&limit=60&display=compact,
+highlight the full page of familiar names, and copy them into owned.txt. You will need to do this for every page individually.
+If you keep any familiars in your vault, you should also check 
+https://www1.flightrising.com/bestiary/{userId}?view=discovered&filter=true&location=vault&bond_level=not-awakened&limit=60&display=compact 
+and copy them as well. (Just paste them into owned.txt along with the others.)
+
+Then, open https://www1.flightrising.com/bestiary/{userId}?view=discovered&filter=true&location=lair&bond_level=not-awakened&limit=60&display=compact,
+highlight all familiar names, and copy them into in-progress.txt.
+WARNING: The site does not distinguish between familiars equipped in the lair vs hibernal den - if your only copy of a familiar required for a feat 
+is currently equipped on a dragon in the hibernal den (and therefore can't be bonded), the script will still not show it.
+
+If the provided copy of feats-list.txt hasn't yet been updated for new feats, you should open 
+https://www1.flightrising.com/forums/gde/3416901#post_58979490, highlight from the first familiar name to the last, and copy it all into feats-list.txt.
+Don't worry about the headings and spacers, the script will take care of filtering those out; just make sure not to copy anything before the first 
+familiar or after the last.
+
+Once you've completed these steps, run the script again to get your queue of fams to be bonded with! :]
+
+(If you have questions about any of this, feel free to contact Schmelios (#676173) through whatever means is convenient to you!)"""
+    
+    sys.exit(instructions)
 
 def removeNicknames(list):
     for fam in list:
         if fam[0] == '“':
             list.remove(fam)
 
-with open("owned.txt", "r") as file:
-    ownedFams = file.read().splitlines()
+def removeBlankLinkes(list):
+    while "" in list:
+        list.remove("")
 
-with open("in-progress.txt", "r") as file:
-    equippedFams = file.read().splitlines()
 
 removeNicknames(ownedFams)
-removeNicknames(equippedFams)
+removeBlankLinkes(ownedFams)
 
+removeNicknames(equippedFams)
+removeBlankLinkes(equippedFams)
 
 
 with open("feats-list.txt", "r") as file:
@@ -55,4 +91,5 @@ for line in featsList:
 
 for x in cleanFeatsList:
     if x in ownedFams and x not in equippedFams:
-        print(x)
+        # print(x)
+        pass
